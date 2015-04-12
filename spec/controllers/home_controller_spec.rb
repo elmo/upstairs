bdbd304611f3fe2654eed7477c4ce23d3917d2d4
Elliott Blatt <elliott@hitfix.com>
2015-04-12 09:39:17 -0700

    added render specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,18 +7,21 @@ RSpec.describe HomeController, :type => :controller do
       get :index
       expect(response).to be_success
       expect(response).to have_http_status(200)
+      expect(response).to render_template("index")
     end
 
     it "responds successfully with an HTTP 200 status code" do
       get :about
       expect(response).to be_success
       expect(response).to have_http_status(200)
+      expect(response).to render_template("about")
     end
 
     it "responds successfully with an HTTP 200 status code" do
       get :contact
       expect(response).to be_success
       expect(response).to have_http_status(200)
+      expect(response).to render_template("contact")
     end
   end
 end
