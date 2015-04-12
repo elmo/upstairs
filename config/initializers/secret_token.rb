08720493232b593b2abd8102ce28ac75a7c95f5d
Elliott Blatt <elliott@hitfix.com>
2015-04-12 09:30:37 -0700

    installed rspec

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Upstairs::Application.config.secret_token = 'c812bca15436e3ccad9e5db65eb4ada010da0e4f9005aa65b9b1a79ab6cecb15138721c05212bc55226a8eccfa6dc80e051eea6e17c17c52831466caa031a2dc'
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe HomeController, :type => :controller do
+  describe "GET #index" do
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :about
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :contact
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+end
