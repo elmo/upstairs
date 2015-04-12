d6484a89185884d5b0ed3764859840736002dbda
Elliott Blatt <elliott@hitfix.com>
2015-04-12 11:19:25 -0700

    two footers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def footer
+    ['home'].include?(controller_name) ? narrow_footer : narrow_footer
+  end
+
+  def big_footer
+    render partial: '/layouts/big_footer'
+  end
+
+  def narrow_footer
+    render partial: '/layouts/narrow_footer'
+  end
+
 end
