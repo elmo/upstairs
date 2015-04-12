84fe9f8ce923c06ec4703124a2e666a72115d405
Elliott Blatt <elliott@hitfix.com>
2015-04-12 13:43:02 -0700

    stubbed out informational pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Upstairs::Application.routes.draw do
   get '/' => "home#index", as: :home
   get '/about' => "home#about", as: :about
   get '/contact' => "home#contact", as: :contact
+  get '/terms' => "home#terms_of_service", as: :terms_of_service
+  get '/privacy' => "home#privacy", as: :privacy
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -23,5 +23,19 @@ RSpec.describe HomeController, :type => :controller do
       expect(response).to have_http_status(200)
       expect(response).to render_template("contact")
     end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :terms_of_service
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+      expect(response).to render_template("terms_of_service")
+    end
+
+    it "responds successfully with an HTTP 200 status code" do
+      get :privacy
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+      expect(response).to render_template("privacy")
+    end
   end
 end
