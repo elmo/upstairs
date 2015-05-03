2e3a668082f794a842caa6c1921db32e3c8458ca
Elliott Blatt <elliott@hitfix.com>
2015-05-02 17:43:01 -0700

    re-configure

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,16 +71,4 @@ Upstairs::Application.configure do
   #
   # As per Devise readme
   config.eager_load = true
-
-  #sendgrid
-  config.action_mailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
-
 end
