c023aa40baafc01dac0a016d026fffa930599f0a
Elliott Blatt <elliott@hitfix.com>
2015-05-18 09:39:06 -0700

    really send invitation email

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -5,6 +5,7 @@ class Invitation < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
   after_create :set_token
+  after_create :send_invitation
 
   def to_param
     token
@@ -14,6 +15,10 @@ class Invitation < ActiveRecord::Base
 
   def set_token
     update_attribute(:token, Digest::MD5.hexdigest("#{id}-#{email}") )
+  end
+
+  def send_invitation
+    InvitationMailer.invite(self)
   end
 
 end
