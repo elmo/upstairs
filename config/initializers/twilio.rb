3738e696e295f9c283fdd53df04c0fd19eff5f6d
Elliott Blatt <elliott@hitfix.com>
2015-05-23 13:18:40 -0700

    update Twilio auth token

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
 Twilio.configure do |config|
   config.account_sid = 'AC56f57f519ed657d077e8798e5dae236f'
-  config.auth_token = '593a132a4834a062a49788b7b106a503'
+  config.auth_token = 'c2cf77d2b04ea7df861b6095cf8906b4'
 end
