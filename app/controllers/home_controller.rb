dc438afdd9c55a58843eee377ed60f0903d9a7b7
Elliott Blatt <elliott@hitfix.com>
2015-07-03 16:05:14 -0700

    restored home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,13 @@ class HomeController < ApplicationController
   layout false, only: [:splash]
 
   def home
-    if current_user.present?
-     redirect_to  current_user.default_building.present? ? building_path(current_user.default_building) : buildings_path
-    elsif current_user.present? and current_user.landlord_or_manager?
-     render template: '/home/home.html.erb'
-    else
-     redirect_to buildings_path
-    end
+    #if current_user.present?
+    # redirect_to  current_user.default_building.present? ? building_path(current_user.default_building) : buildings_path
+    #elsif current_user.present? and current_user.landlord_or_manager?
+    # render template: '/home/home.html.erb'
+    #else
+    # redirect_to buildings_path
+    #end
   end
 
   def splash
