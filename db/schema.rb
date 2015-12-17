f8b3e782c58cbbaaa4549635aea8039abfe3407e
Elliott Blatt <elliott.blatt@gmail.com>
2015-12-17 11:15:31 -0800

    added color column to category

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211193610) do
+ActiveRecord::Schema.define(version: 20151217182451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20151211193610) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.string   "slug"
+    t.string   "color"
   end
 
   add_index "categories", ["slug"], name: "index_categories_on_slug", unique: true, using: :btree
--- spec/factories/category.rb
+++ spec/factories/category.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :category do
     name 'name'
+    color 'red'
   end
 end
