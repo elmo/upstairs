61beb448c72f4e2d9b0e2febf86648cb09a5898a
Elliott Blatt <elliott.blatt@gmail.com>
2015-12-19 12:30:33 -0800

    revert

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,16 @@
 class HomeController < ApplicationController
-  layout 'users'
+  layout false, only: [:splash]
+
+  def home
+    # if current_user.present?
+    # redirect_to  current_user.default_building.present? ? building_path(current_user.default_building) : buildings_path
+    # elsif current_user.present? and current_user.landlord_or_manager?
+    # render template: '/home/home.html.erb'
+    # else
+    # redirect_to buildings_path
+    # end
+  end
 
   def splash
   end
-
 end
