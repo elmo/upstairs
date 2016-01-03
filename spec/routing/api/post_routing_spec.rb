069eae3a3e53a72085c0dd4025741f83674ec977
Elliott Blatt <elliott.blatt@gmail.com>
2016-01-03 07:50:22 -0800

    added tips api and tests

--- spec/routing/api/post_routing_spec.rb
+++ spec/routing/api/post_routing_spec.rb
@@ -10,5 +10,9 @@ RSpec.describe Api::PostsController, type: :routing do
       expect(get: 'api/buildings/1/posts/1').to route_to('api/posts#show', id: "1", building_id: "1" )
     end
 
+    it 'routes to #tips' do
+      expect(get: 'api/buildings/1/posts/tips').to route_to('api/posts#tips', building_id: "1" )
+    end
+
   end
 end
