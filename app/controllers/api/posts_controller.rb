a82a1e5ab942f2e0fb7c7c8f5135c66ae42c0f09
Elliott Blatt <elliott.blatt@gmail.com>
2016-01-03 07:50:22 -0800

    added tips api and tests

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,8 +1,14 @@
 class Api::PostsController < ApplicationController
-  before_action :get_building, only: [:index,:show]
+  before_action :get_building, only: [:index,:show,:tips]
 
   def index
     @posts = @building.posts.page(params[:page]).order(created_at: :desc).per(4)
+    render json: @posts
+  end
+
+  def tips
+    category = Category.find_by_name(Category::CATEGORY_TIPS)
+    @posts = @building.posts.where(category_id: category.id ).page(params[:page]).order(created_at: :desc).per(4)
     render json: @posts
   end
 
